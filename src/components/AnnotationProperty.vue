5d4d09b437fa1379933bf9ae97dc735ef3b638b6
Adrien Boucaud <adrien.boucaud@kitware.com>
2021-09-23 15:08:47 +0200

    Add skeleton for properties store

---
+++ src/components/AnnotationProperty.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <v-subheader>
+      Computed Annotation Properties
+    </v-subheader>
+    <v-row v-for="property in properties" :key="property.id">
+      <v-col></v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Watch, Prop } from "vue-property-decorator";
+import store from "@/store";
+import propertyStore from "@/store/properties";
+
+@Component({})
+export default class AnnotationProperties extends Vue {
+  readonly store = store;
+  readonly propertyStore = propertyStore;
+
+  get properties() {
+    return propertyStore.properties;
+  }
+}
+</script>
